6fd79d357341bac18141c3c91f6233c5ea5d0b0b
kevinrisqi22@gmail.com <kevinrisqi222>
2020-04-02 01:10:11 +0700

    Added file Recipe.js

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ const App = () => {
   const APP_ID = "801c88f9";
   const APP_KEY = "eade02e66b8e503a9f76880ee01b2250";
 
-  useEffect(() => {
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect(() => {
+    getRecipes();
   }, []);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    
+    setRecipes(data.hits);
   }
 
   return (
